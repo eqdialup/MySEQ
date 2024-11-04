5ea6df0159736b42f06fa4d2cf4bee9d0b5a3e42
Lenothx <better.was.eprinz@gmail.com>
2024-11-04 19:36:27 +0100

    small clean

--- client/Enums.cs
+++ client/Enums.cs
@@ -54,50 +54,6 @@ namespace Structures
         DrawAll = 0x0FFFFFFF  // Maximum possible combination
     }
 
-    /*public enum DrawOptions
-    {
-        /// <summary>
-        /// Maximum savings - all enabled
-        /// </summary>
-        None = 0x00000000,
-
-        DrawMap = 0x00000001,
-        Readjust = 0x00000002,
-        Player = 0x00000004,
-        SpotLine = 0x00000008,
-        Spawns = 0x00000010,
-        SpawnTrails = 0x00000020,
-        GroundItems = 0x00000040,
-        SpawnTimers = 0x00000080,
-        DirectionLines = 0x00000100,
-        SpawnRings = 0x00000200,
-        GridLines = 0x00000400,
-
-        DrawEvenLess = DrawLess          // Takes away substantially
-                                      - Readjust,
-
-        DrawLess = DrawNormal        // Takes away more uncritically things
-                                      - DirectionLines
-                                      - GroundItems
-                                      - SpawnTimers
-                                      - ZoneText,
-
-        ZoneText = 0x00000800,
-
-        DrawNormal = DrawMap           // Standard, nice, savings
-                                      + Readjust
-                                      + Player
-                                      + SpotLine
-                                      + Spawns
-                                      + GroundItems
-                                      + SpawnTimers
-                                      + DirectionLines
-                                      + GridLines
-                                      + ZoneText,
-
-        DrawAll = 0x0fffffff,
-    }*/
-
     public enum FollowOption
     {
         None,
